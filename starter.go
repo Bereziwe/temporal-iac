020977e2cc5bcd7148225d94d851e5079e3acf73
Miza <Bereziwe.Miza@lumen.com>
2025-06-26 16:29:17 -0500

    new update

---
+++ starter.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "context"
+    "log"
+    "time"
+
+    "go.temporal.io/sdk/client"
+    "your/module/path/workflows" // Replace with the actual module path
+)
+
+func main() {
+    // Create Temporal client
+    c, err := client.NewClient(client.Options{})
+    if err != nil {
+        log.Fatalln("Unable to create Temporal client", err)
+    }
+    defer c.Close()
+
+    // Define workflow options
+    workflowOptions := client.StartWorkflowOptions{
+        ID:        "my_workflow_id",
+        TaskQueue: "terraform-task2",
+        Timeout:   time.Minute * 10,
+    }
+
+    // Start the workflow
+    we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, workflows.MyWorkflow, "Hello from starter.go!")
+    if err != nil {
+        log.Fatalln("Unable to execute workflow", err)
+    }
+
+    log.Println("Started workflow", "WorkflowID:", we.GetID(), "RunID:", we.GetRunID())
+}
