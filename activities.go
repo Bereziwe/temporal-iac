756cebe7ccf54635a098b3518ecafc587d23841f
Miza <Bereziwe.Miza@lumen.com>
2025-06-26 11:56:31 -0500

    new update

--- activities.go
+++ activities.go
@@ -7,17 +7,17 @@ import (
 )
 
 
-func TerraformInitActivity(ctx context.Context) error {
-    cmd := exec.Command("terraform", "init")
-	cmd.Dir = "/home/runner/work/temporal-iac/temporal-iac/terraform"
-    // cmd.Dir = "./terraform" // Replace with your actual path
-    output, err := cmd.CombinedOutput()
-    if err != nil {
-        return fmt.Errorf("terraform init failed: %v\nOutput: %s", err, string(output))
-    }
+// func TerraformInitActivity(ctx context.Context) error {
+//     cmd := exec.Command("terraform", "init")
+// 	cmd.Dir = "/home/runner/work/temporal-iac/temporal-iac/terraform"
+//     // cmd.Dir = "./terraform" // Replace with your actual path
+//     output, err := cmd.CombinedOutput()
+//     if err != nil {
+//         return fmt.Errorf("terraform init failed: %v\nOutput: %s", err, string(output))
+//     }
 
-    return nil
-}
+//     return nil
+// }
 
 func TerraformApply(ctx context.Context, config string) (string, error) {
     fmt.Println("Executing Terraform with config:", config)
