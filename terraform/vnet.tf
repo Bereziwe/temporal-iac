662bb254726ec5a1d8f597bd7152b2d022e3d272
Miza <Bereziwe.Miza@lumen.com>
2025-06-26 08:39:47 -0500

    update

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -1,5 +1,23 @@
 
-resource "azurerm_resource_group" "vnet" {
-  name     = "myVnet-RG"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test-rg" {
+  name     = "example-resources"
   location = "East US"
+}
+
+resource "azurerm_virtual_network" "test-vnet" {
+  name                = "test-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test-rg.location
+  resource_group_name = azurerm_resource_group.test-rg.name
+}
+
+resource "azurerm_subnet" "test-subnet" {
+  name                 = "test-subnet"
+  resource_group_name  = azurerm_resource_group.test-rg.name
+  virtual_network_name = azurerm_virtual_network.test-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
