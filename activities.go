d5aeb32806d4e24bc85376a7b40c5cc05aa80a4d
Miza <Bereziwe.Miza@lumen.com>
2025-06-26 11:14:05 -0500

    new update

--- activities.go
+++ activities.go
@@ -5,6 +5,13 @@ import (
     "fmt"
 )
 
+
+func InitTerraform() error {
+cmd := exec.Command("terraform", "init")
+cmd.Dir = "./terraform"
+return cmd.Run()
+}
+
 func TerraformApply(ctx context.Context, config string) (string, error) {
     fmt.Println("Executing Terraform with config:", config)
     // Simulate Terraform logic or call out to scripts/binaries
