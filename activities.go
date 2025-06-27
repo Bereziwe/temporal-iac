4411c1ea4a17ee7bcbb7613a198556fc6f922974
Miza <Bereziwe.Miza@lumen.com>
2025-06-27 09:09:29 -0500

    new update

--- activities.go
+++ activities.go
@@ -3,8 +3,33 @@ package main
 import (
     "context"
     "fmt"
+    "log"
+    "go.temporal.io/sdk/client"
+    "go.temporal.io/sdk/worker"
 	// "os/exec"
 )
+
+func main() {
+    c, err := client.NewClient(client.Options{
+		HostPort: "172.172.245.99:7233",
+	})
+    if err != nil {
+        log.Fatalln("Unable to create Temporal client", err)
+    }
+    defer c.Close()
+
+    w := worker.New(c, "terraform-task2", worker.Options{})
+
+    w.RegisterWorkflow(TerraformWorkflow)
+    w.RegisterActivity(TerraformApply)
+	// w.RegisterActivity(TerraformInit)
+
+    log.Println("Worker started for task queue: terraform-task2")
+    err = w.Run(worker.InterruptCh())
+    if err != nil {
+        log.Fatalln("Unable to start worker", err)
+    }
+}
 
 
 // func TerraformInit(ctx context.Context) error {
